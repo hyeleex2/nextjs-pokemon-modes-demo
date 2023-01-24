c48bff25bd5012aed950ef9f8ff3bcf72cf65caf
hl.lee@quintet.co.kr <hyeleex2@gmail.com>
2023-01-24 21:44:53 +0900

    ssr

---
+++ pages/pokemon/[id].tsx
@@ -0,0 +1,72 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from "@/styles/Details.module.css";
+// import { Head } from "next/document";
+import Link from "next/link";
+import { GetServerSidePropsContext } from "next";
+
+type Stats = {
+  name: string;
+  value: number;
+};
+interface Pokemon {
+  name: string;
+  type: string[];
+  stats: Stats[];
+  image: string;
+}
+
+export async function getServerSideProps({
+  params,
+}: GetServerSidePropsContext) {
+  const resp = await fetch(
+    `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
+  );
+  return {
+    props: {
+      pokemon: await resp.json(),
+    },
+  };
+}
+
+export default function Details({ pokemon }: Pokemon) {
+  return (
+    <div>
+      {/* <Head>
+        <title>{pokemon?.name}</title>
+      </Head> */}
+      <div>
+        <Link href="/">Back to home</Link>
+      </div>
+      <div className={styles.layout}>
+        <div>
+          <img
+            className={styles.picture}
+            src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon?.image}`}
+            alt={pokemon?.name}
+          />
+        </div>
+        <div>
+          <div className={styles.name}>{pokemon?.name}</div>
+          <div className={styles.type}>{pokemon?.type.join(", ")}</div>
+          <table>
+            <thead className={styles.header}>
+              <tr>
+                <th>Name</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon?.stats.map(({ name, value }) => (
+                <tr key={name}>
+                  <td className={styles.attribute}>{name}</td>
+                  <td>{value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
